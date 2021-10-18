67d3e992fc177a79f5e89a627bc1c619b6d411bc
RINEARN <rinearn@gmail.com>
2021-10-18 18:19:29 +0900

    Add: new class: Exevalator
    The class of the interpreter engine of Exevalator.

---
+++ Exevalator.java
@@ -0,0 +1,21 @@
+/*
+ * Put this code in your source code folder, and write the package-statement as follows.
+ */
+// package your.projects.package.anywhere;
+
+
+/**
+ * Interpreter Engine of Exevalator.
+ */
+public class Exevalator {
+
+	/**
+	 * Evaluates (computes) the value of an expression.
+	 *
+	 * @param expression The expression to be evaluated
+	 * @return The evaluated value
+	 */
+	public double eval(String expression) {
+		return Double.NaN;
+	}
+}
