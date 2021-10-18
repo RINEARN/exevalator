8ea20a893b54e17b763f815aadf56ded18ba0a8f
RINEARN <rinearn@gmail.com>
2021-10-19 08:51:21 +0900

    Add: new class: Operator

--- Exevalator.java
+++ Exevalator.java
@@ -1,4 +1,9 @@
 /*
+ * Copyright(C) 2021 RINEARN (Fumihiro Matsui)
+ * This software is released under the MIT License.
+ */
+
+/*
  * Put this code in your source code folder, and write the package-statement as follows.
  */
 // package your.projects.package.anywhere;
@@ -17,5 +22,54 @@ public class Exevalator {
 	 */
 	public double eval(String expression) {
 		return Double.NaN;
+	}
+
+
+	/**
+	 * The class storing information of an operator.
+	 */
+	private static class Operator {
+
+		/**
+		 * The enum representing types of operators.
+		 */
+		private static enum Type {
+
+			/** Represents unary operator, for example: - of -1.23 */
+			UNARY,
+
+			/** Represents binary operator, for example: + of 1+2 */
+			BINARY,
+		}
+
+		/** The symbol of this operator (for example: '+'). */
+		public final String symbol;
+
+		/** The precedence of this operator (smaller value gives higher precedence). */
+		public final int precedence;
+
+		/** The type of operator tokens. */
+		public final Type type;
+
+		/**
+		 * Create an Operator instance storing specified information.
+		 *
+		 * @param type The type of this operator
+		 * @param symbol The symbol of this operator
+		 * @param precedence The precedence of this operator
+		 */
+		public Operator(Type type, String symbol, int precedence) {
+			this.type = type;
+			this.symbol = symbol;
+			this.precedence = precedence;
+		}
+
+		/**
+		 * Returns the String representation of this Operator instance.
+		 */
+		@Override
+		public String toString() {
+			return "Operator [symbol=" + symbol + ", precedence=" + precedence + ", type=" + type + "]";
+		}
 	}
 }
