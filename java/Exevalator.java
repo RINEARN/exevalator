687e6c456a72d073782cf91bd012030b1da889f4
RINEARN <commit@rinearn.com>
2024-07-14 00:55:57 +0900

    Update: version: 2.0.0

--- java/Exevalator.java
+++ java/Exevalator.java
@@ -1,5 +1,5 @@
 /*
- * Exevalator Ver.2.0.-1 - by RINEARN 2021-2024
+ * Exevalator Ver.2.0.0 - by RINEARN 2021-2024
  * This software is released under the "Unlicense" license.
  * You can choose the "CC0" license instead, if you want.
  */
